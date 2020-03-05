8e47d01ac3b4526a4584293bfe822f8dff515ff3
MagiX7 <DavidGonzalez2001-9@hotmail.com>
2020-03-05 15:56:14 +0100

    SDL_MiniGame

---
+++ SDL_MiniGame/Game.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "SDL/include/SDL.h"
+#pragma comment(lib, "SDL/libx86/SDL2.lib")
+#pragma comment (lib, "SDL/libx86/SDL2main.lib")
+#include "SDL_Image/include/SDL_image.h"
+#pragma comment( lib, "SDL_Image/libx86/SDL2_image.lib" )
+
+#include "SDL_Mixer/include/SDL_mixer.h"
+#pragma comment(lib, "SDL_Mixer/libx86/SDL2_mixer.lib")
+
+#include "Entity.h"
